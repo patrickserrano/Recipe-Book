4b46042808f9ef00fe8a06f62242aed379803e8c
Patrick Serrano <hello@patrickserrano.com>
2022-02-21 14:08:15 -0500

    Initial Commit

---
+++ Recipe Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipe Book
+//
+//  Created by Patrick Serrano on 2/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe Book/Recipe_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BookApp.swift
+//  Recipe Book
+//
+//  Created by Patrick Serrano on 2/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
